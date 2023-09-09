e7b55625bd0181c602b1cc9e8b7cb0800c10ebfe
Jayjeet1013 <jayjeet1013@gmail.com>
2023-09-09 23:38:35 +0530

    structring

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from '@/layout/Navbar'
+import Footer from '@/layout/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Navbar/>
+    <Component {...pageProps} /> 
+    <Footer/>
+    </ChakraProvider>
+   
+  )
 }
