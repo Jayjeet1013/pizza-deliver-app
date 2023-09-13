acc861c0292a51fe9dc0da7e51836750981f1f61
Jayjeet1013 <jayjeet1013@gmail.com>
2023-09-13 22:40:23 +0530

    nabvar started

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,8 +1,26 @@
+import { Box, Container, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <Box>
+    <Container maxW={1300}>
+    <Flex justifyContent={'space-between'}>
+   <Flex>
+    Logo
+   </Flex>
+      <Flex  direction={{base:'column',md:'row'}} justifyContent={'space-between'}>
+      <Text>Contact</Text>
+      <Text>Blog</Text>
+      <Text>HomePage</Text>
+      <Text>Events</Text>
+      <Text>Menu</Text>
+      <Text>Products</Text>
+      </Flex>
+      <Box>Icon</Box>
+    </Flex>
+    </Container>
+   </Box>
   )
 }
 
